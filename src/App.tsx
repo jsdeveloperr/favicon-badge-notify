6ffa3094a686f46c7a31ff0ded97bb5e8317aed0
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-06-15 23:45:53 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { Helmet } from "react-helmet"
+
+import logo from './logo.svg'
+import './App.css'
+import useFaviconBadge from './hooks/use-favicon-badge'
+// import FaviconBadge from './components/favicon-badge'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [favicon, setFavicon] = useState('/src/assets/favicon.svg')
+  const { drawBadge } = useFaviconBadge({
+    src: '/src/assets/favicon.svg',
+    badgeValue: count,
+  })
+
+  useEffect(() => {
+    drawBadge().then(badge => setFavicon(badge));
+  }, [count]);
+
+  return (
+    <div className="App">
+      <Helmet>
+        <link rel="icon" type="image/png" sizes="128x128" href={favicon}></link>
+      </Helmet>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>To send notifications to your browser tab, increase the number.</p>
+        <p className="buttons">
+          <button type="button" className="increase" onClick={() => setCount((count) => count + 1)}>
+            increase
+          </button>
+          <span>{count}</span>
+          <button type="button" className="decrease" onClick={() => count - 1 >= 0 && setCount((count) => count - 1)}>
+            decrease
+          </button>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/hooks/use-favicon-badge.ts
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function useFaviconBadge ({
+  src,
+  badgeValue,
+  backgroundColor = "red",
+  textColor = "white",
+}: {
+  src: string;
+  badgeValue: any;
+  backgroundColor?: string;
+  textColor?: string;
+}) {
+  let faviconSize = 96;
+  const canvas = document.createElement("canvas");
+  canvas.width = faviconSize;
+  canvas.height = faviconSize;
+
+  const context = canvas.getContext("2d");
+  const img = document.createElement("img");
+  img.setAttribute("src", src);
+
+  const drawBadge: () => Promise<string> = () => {    
+    return new Promise((resolve) => {
+      img.addEventListener('load', () => {
+        if (badgeValue && context) {
+          context.drawImage(img, 0, 0, faviconSize, faviconSize);
+  
+          // Draw Badge Circle
+          context.beginPath();
+          context.arc(
+            canvas.width - faviconSize / 3,
+            faviconSize / 3,
+            faviconSize / 3,
+            0,
+            2 * Math.PI
+          );
+          context.fillStyle = backgroundColor;
+          context.fill();
+  
+          // Draw Badge Value
+          context.font = '55px "helvetica", sans-serif';
+          context.textAlign = "center";
+          context.textBaseline = "middle";
+          context.fillStyle = textColor;
+          context.fillText(badgeValue, canvas.width - faviconSize / 3, faviconSize / 3);
+          
+          resolve(canvas.toDataURL("image/png"))
+        } else {
+          resolve(src);
+        }
+      })
+    })
+  }
+
+  return {
+    drawBadge,
+  };
+}
