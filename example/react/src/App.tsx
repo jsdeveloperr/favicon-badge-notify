2cb56a183fd43252807221fa587137b1c2db34ca
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-06-16 21:20:36 +0300

    vue example added

--- example/react/src/App.tsx
+++ example/react/src/App.tsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 
 import logo from './logo.svg'
 import './App.css'
-import useFaviconBadge from '../../src'
+import useFaviconBadge from '../../../src'
 import faviconSvg from "./assets/favicon.svg";
 
 function App() {
---
+++ example/vue/src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Head } from '@vueuse/head'
+import useFaviconBadge from '../../../src'
+
+const favicon = ref('/favicon.svg');
+const count = ref(0);
+
+const setFavicon = (val: string) => {
+  favicon.value = val
+};
+
+const setCount = (val: number) =>  {
+ count.value = val;
+}
+
+const { drawBadge } = useFaviconBadge({
+  src: favicon.value,
+  badgeValue: count.value,
+})
+
+drawBadge().then(badge => setFavicon(badge));
+
+defineProps<{ msg: string }>()
+
+</script>
+
+
+
+<template>
+  <Head>
+    <title>Vue Badge Favicon</title>
+    <link rel="icon" type="image/png" sizes="128x128" :href="favicon" />
+  </Head>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="setCount(count + 1)">count is: {{ count }}</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
