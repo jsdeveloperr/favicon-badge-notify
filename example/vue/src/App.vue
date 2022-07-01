cc1e4f612ff6bfc6536fc7d344f26ff2f53da775
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-07-01 22:47:47 +0300

    vue example changed

--- example/vue/src/App.vue
+++ example/vue/src/App.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
-import { ref, onBeforeUnmount } from 'vue'
+import { ref, onBeforeUnmount, reactive, onMounted } from 'vue'
 import { Head } from '@vueuse/head'
 import useFaviconBadge from '../../../src'
 
+const state = reactive({
+  count: 0,
+  favicon: 'favicon.svg'
+});
 
-const favicon = ref('/favicon.svg');
-const count = ref(0);
-
-const setFavicon = (val: string) => {
-  favicon.value = val
-};
-
-const setCount = (val: number) =>  {
-  count.value = val;
-}
+const setFavicon = (val: string) => state.favicon = val;
+const setCount = (val: number) =>  state.count = val;
 
 const { drawBadge, destroyBadge } = useFaviconBadge({
-  src: favicon.value,
-  badgeValue: count.value,
+  src: state.favicon,
+  badgeValue: state.count,
 })
 
-const clicked = () => {
-  setCount(count.value + 1)
-  drawBadge().then(badge => setFavicon(badge))
-}
-
-drawBadge().then(badge => { console.log(badge); setFavicon(badge)});
+onMounted(() => {
+ drawBadge().then(badge => setFavicon(badge));  
+})
 
 defineProps<{ msg: string }>()
 
@@ -35,16 +28,18 @@ onBeforeUnmount(() => {
 
 </script>
 
-
-
 <template>
   <Head>
     <title>Vue Badge Favicon</title>
-    <link rel="icon" type="image/png" sizes="128x128" :href="favicon" />
+    <link rel="icon" type="image/png" sizes="128x128" :href="state.favicon" />
   </Head>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>{{ msg }}</h1>
-  <button type="button" @click="clicked()">count is: {{ count }}</button>
+  <p>To send <code>notifications</code> to your browser tab, increase the number. {{ msg }}</p>
+          <p class="buttons">
+  <button type="button" class="increase" @click="setCount(state.count + 1)">increase</button>
+  <span>{{ state.count }}</span>
+  <button type="button" class="decrease" @click="state.count - 1 >= 0 && setCount(state.count - 1)">decrease</button>
+  </p>
 </template>
 
 <style>
@@ -56,4 +51,28 @@ onBeforeUnmount(() => {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+  font-size: calc(10px + 2vmin);
+  border-radius: 4px;
+  padding: 0 8px;
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 16px;
+}
+
+.increase {
+  border: 2px solid #304455;
+  color: #304455;
+}
+
+.decrease {
+  border: 2px solid #42b983;
+  color: #42b983;
+}
+
 </style>
