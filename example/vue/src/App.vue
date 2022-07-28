328089a4fa1acfdc1caa331561dadaa1e9a73ccd
Abdulnasır Olcan <abdulnasirolcan@Abdulnasr-Olcan-MacBook-Pro.local>
2022-07-28 12:56:18 +0300

    🚀 vue example editted

--- example/vue/src/App.vue
+++ example/vue/src/App.vue
@@ -1,37 +1,31 @@
 <script setup lang="ts">
-import { ref, onBeforeUnmount, watch } from 'vue'
-import { Head } from '@vueuse/head'
-import useFaviconBadgeNotify from '../../../src'
-
+import { ref, onBeforeUnmount, watch } from 'vue';
+import { Head } from '@vueuse/head';
+import useFaviconBadgeNotify from '../../../src';
 
 const favicon = ref('/favicon.svg');
 const count = ref(0);
 
 const setFavicon = (val: string) => {
-  favicon.value = val
+  favicon.value = val;
 };
 
-const setCount = (val: number) =>  {
+const setCount = (val: number) => {
   count.value = val;
-}
+};
 
 const { drawBadge, destroyBadge } = useFaviconBadgeNotify({
-  src: favicon.value,
-})
+  src: favicon.value
+});
 
 watch(count, (count, prevCount) => {
-  drawBadge(count).then(badge => setFavicon(badge));
-})
+  drawBadge(count).then((badge) => setFavicon(badge));
+});
 
 onBeforeUnmount(() => {
   destroyBadge();
-})
-
-defineProps<{ msg: string }>()
-
+});
 </script>
-
-
 
 <template>
   <Head>
@@ -39,8 +33,19 @@ defineProps<{ msg: string }>()
     <link rel="icon" type="image/png" sizes="128x128" :href="favicon" />
   </Head>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>{{ msg }}</h1>
-  <button type="button" @click="setCount(count + 1)">count is: {{ count }}</button>
+  <p class="buttons">
+    <button type="button" class="increase" @click="setCount(count + 1)">increase</button>
+    <span>{{ count }}</span>
+    <button type="button" class="decrease" @click="count - 1 >= 0 && setCount(count - 1)">
+      decrease
+    </button>
+  </p>
+  <footer>
+    <p>
+      🍁 MIT Licensed | Copyright © 2022-present Abdulnasır Olcan and @favicon-badge-notify
+      contributors
+    </p>
+  </footer>
 </template>
 
 <style>
@@ -51,5 +56,33 @@ defineProps<{ msg: string }>()
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  font-size: calc(10px + 2vmin);
+  border-radius: 4px;
+  padding: 0 8px;
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 16px;
+}
+
+.increase {
+  border: 2px solid #42b983;
+  color: #42b983;
+}
+
+.decrease {
+  border: 2px solid #304455;
+  color: #304455;
+}
+
+span {
+  color: #42b983;
+  font-size: 24px;
 }
 </style>
